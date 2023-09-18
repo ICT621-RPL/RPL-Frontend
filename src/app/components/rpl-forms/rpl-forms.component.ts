1572d9f9def2f8493839d3bcf7559aac98718d50
Manish976 <shakyamanish978@gmail.com>
2023-09-19 01:26:47 +0800

    changes on design

--- src/app/components/rpl-forms/rpl-forms.component.ts
+++ src/app/components/rpl-forms/rpl-forms.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AddExperienceComponent } from './../add-experience/add-experience.component';
 
 import {MatDialog} from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-rpl-forms',
@@ -10,20 +11,36 @@ import {MatDialog} from '@angular/material/dialog';
 })
 export class RplFormsComponent implements OnInit {
 
+  months: string[]= []
+  years: number[] = [];
+  public form = this.fb.group({
+        jobTitle: ['', Validators.required]
+      });
   public closeResult = '';
-  constructor(private modalService: MatDialog) { }
+  constructor(private fb: FormBuilder) { 
+     
+      const currentYear = new Date().getFullYear();
+      const startYear = 1980; 
+      for (let year = currentYear; year >= startYear; year--) {
+        this.years.push(year);
+      }
+      this.months = [
+      'January', 'February', 'March', 'April', 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December'
+    ];
+  }
 
   ngOnInit(): void {
   }
 
-  public openModal(): void {
-    this.modalService.open(AddExperienceComponent, {
-       position: {
-        top: '0',
-        right: '0',
-      },
-      width: '500px',
-      height: '100%',
-    })
-	}
+  // public openModal(): void {
+  //   this.modalService.open(AddExperienceComponent, {
+  //      position: {
+  //       top: '0',
+  //       right: '0',
+  //     },
+  //     width: '500px',
+  //     height: '100%',
+  //   })
+	// }
 }
