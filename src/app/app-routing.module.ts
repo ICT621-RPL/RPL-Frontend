5889c3fb651f19a723e85f20e8d3026dd64bdf32
Manish976 <shakyamanish978@gmail.com>
2023-10-10 12:42:39 +0800

    removed auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { RplFormsComponent } from './components/rpl-forms/rpl-forms.component';
 import { UploadDocumentsComponent } from './components/upload-documents/upload-documents.component';
 import { SubmitSuccessComponent } from './components/submit-success/submit-success.component';
-import { MsalGuard } from '@azure/msal-angular';
 
 const routes: Routes = [
-  { path: '', component: RplFormsComponent, canActivate: [
-      MsalGuard
-    ]},
+  { path: '', component: RplFormsComponent},
   { path: 'upload', component: UploadDocumentsComponent, pathMatch: 'full'  },
   { path: 'submit', component: SubmitSuccessComponent },
   { path: '', redirectTo: '/', pathMatch: 'full' } // redirect to `home` component
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,54 +19,7 @@ import { ProgressComponent } from './components/progress/progress.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { UploadDocumentsComponent } from './components/upload-documents/upload-documents.component';
 import { SubmitSuccessComponent } from './components/submit-success/submit-success.component';
-import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalBroadcastService, MsalGuard, MsalGuardConfiguration, MsalInterceptor, MsalInterceptorConfiguration, MsalModule, MsalRedirectComponent, MsalService } from '@azure/msal-angular';
-import { BrowserCacheLocation, IPublicClientApplication, InteractionType, LogLevel, PublicClientApplication } from '@azure/msal-browser';
 
-const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
-
-export function loggerCallback(logLevel: LogLevel, message: string) {
-  console.log(message);
-}
-
-export function MSALInstanceFactory(): IPublicClientApplication {
-  return new PublicClientApplication({
-    auth: {
-      clientId: 'd937dd6b-3f2e-45b7-9682-17838719d513',
-      authority: 'https://login.microsoftonline.com/consumers',
-      redirectUri: 'http://localhost:4200/'
-    },
-    cache: {
-      cacheLocation: BrowserCacheLocation.LocalStorage,
-      storeAuthStateInCookie: isIE, // set to true for IE 11
-    },
-    system: {
-      loggerOptions: {
-        loggerCallback,
-        logLevel: LogLevel.Info,
-        piiLoggingEnabled: false
-      }
-    }
-  });
-}
-
-export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
-  const protectedResourceMap = new Map<string, Array<string>>();
-  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']);
-
-  return {
-    interactionType: InteractionType.Redirect,
-    protectedResourceMap
-  };
-}
-
-export function MSALGuardConfigFactory(): MsalGuardConfiguration {
-  return { 
-    interactionType: InteractionType.Redirect,
-    authRequest: {
-      scopes: ['user.read']
-    }
-  };
-}
 @NgModule({
   declarations: [
     AppComponent,
@@ -91,29 +44,9 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     MatDialogModule,
     QuillModule.forRoot(),
     HttpClientModule,
-    MsalModule,
 
   ],
-  providers: [ {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MsalInterceptor,
-      multi: true
-    },
-    {
-      provide: MSAL_INSTANCE,
-      useFactory: MSALInstanceFactory
-    },
-    {
-      provide: MSAL_GUARD_CONFIG,
-      useFactory: MSALGuardConfigFactory
-    },
-    {
-      provide: MSAL_INTERCEPTOR_CONFIG,
-      useFactory: MSALInterceptorConfigFactory
-    },
-    MsalService,
-    MsalGuard,
-    MsalBroadcastService],
-  bootstrap: [AppComponent, MsalRedirectComponent]
+  providers: [ ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
